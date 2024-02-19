154d4b110a48a21a0a085870de31fe2c0dbc5b06
omarfaruk1820 <omarfarukfci9th@gmil.comm>
2024-02-19 07:35:12 -0800

    add js file

---
+++ index.js
@@ -0,0 +1,24 @@
+const hourEl=document.querySelector('.hour')
+const minuteE1=document.querySelector('.minute')
+const secondE1=document.querySelector('.second')
+
+
+function updateClock(){
+const currentDate=new Date()
+setTimeout(updateClock,1000);
+
+ const hour=currentDate.getHours()
+ const minute=currentDate.getMinutes()
+const second =currentDate.getSeconds()
+
+const hourDeg=(hour/12)*360;
+hourEl.style.transform=`rotate(${hourDeg}deg)`;
+
+const minuteDeg=(minute/60)*360;
+minuteE1.style.transform=`rotate(${minuteDeg}deg)`;
+
+const secondDeg=(second/60)*360;
+secondE1.style.transform=`rotate(${secondDeg}deg)`;
+ 
+}
+updateClock()
